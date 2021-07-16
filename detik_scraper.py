43ef170e737346c7014ff2f0ea44842d602d4062
Iqbal <mcballz@gmail.com>
2021-07-16 07:59:50 +0700

    belajar scraping

---
+++ detik_scraper.py
@@ -0,0 +1,8 @@
+import requests
+import bs4
+
+url = 'https://www.detik.com/terpopuler?tag_from=wp_cb_mostPopular_more'
+html_doc = requests.get(url, params=('tag_from: wp_cb_mostPopular_more'))
+sup = bs4.BeautifulSoup(html_doc.text, "html.parser")
+popular_area = sup.find(attrs=('class:grid-row list-content'))
+print(popular_area)
